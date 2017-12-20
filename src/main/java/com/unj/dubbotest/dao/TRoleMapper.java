2be83a8a4e1c972e71abfacc0426695f3e84ec25
zhangzhufu <zhangzhufu12@163.com>
2017-12-20 14:41:50 +0800

    初始导入

--- src/main/java/com/unj/dubbotest/dao/TRoleMapper.java
+++ src/main/java/com/unj/dubbotest/dao/TRoleMapper.java
@@ -6,17 +6,17 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 public interface TRoleMapper {
-    int countByExample(TRoleExample example);
+	int countByExample(TRoleExample example);
 
-    int deleteByExample(TRoleExample example);
+	int deleteByExample(TRoleExample example);
 
-    int insert(TRole record);
+	int insert(TRole record);
 
-    int insertSelective(TRole record);
+	int insertSelective(TRole record);
 
-    List<TRole> selectByExample(TRoleExample example);
+	List<TRole> selectByExample(TRoleExample example);
 
-    int updateByExampleSelective(@Param("record") TRole record, @Param("example") TRoleExample example);
+	int updateByExampleSelective(@Param("record") TRole record, @Param("example") TRoleExample example);
 
-    int updateByExample(@Param("record") TRole record, @Param("example") TRoleExample example);
+	int updateByExample(@Param("record") TRole record, @Param("example") TRoleExample example);
 }
