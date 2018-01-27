2444f6f1f8753c7ddd10844981f82f1d4e389d24
zhangzhufu <zhangzhufu12@163.com>
2018-01-27 15:03:19 +0800

    提交

--- src/main/java/com/unj/dubbotest/service/impl/RoleServiceImpl.java
+++ src/main/java/com/unj/dubbotest/service/impl/RoleServiceImpl.java
@@ -1,8 +1,11 @@
 package com.unj.dubbotest.service.impl;
 
 import java.util.List;
+import java.util.concurrent.Future;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 
 import com.unj.dubbotest.dao.TRoleMapper;
 import com.unj.dubbotest.model.TRole;
@@ -15,9 +18,25 @@ public class RoleServiceImpl implements RoleService {
 	@Autowired
 	private TRoleMapper tRoleMapper;
 
+	@Async("logExecutor") 
 	public List<TRole> getRoles() {
 		TRoleExample example = new TRoleExample();
 		List<TRole> roles = tRoleMapper.selectByExample(example);
 		return roles;
 	}
+	
+	@Async  
+	public Future<String> asyncMethodWithReturnType() {  
+	    System.out.println("Execute method asynchronously - "  
+	      + Thread.currentThread().getName());  
+	    try {  
+	        Thread.sleep(5000);  
+	        return new AsyncResult<String>("hello world !!!!");  
+	    } catch (InterruptedException e) {  
+	        //  
+	    }  
+	   
+	    return null;  
+	}
+	
 }
